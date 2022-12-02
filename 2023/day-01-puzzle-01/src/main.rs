ce619481757c9b52961d6e655cb6c1a348234782
jcleira <jmc.leira@gmail.com>
2022-12-02 17:04:31 +0100

    2023 - Day #01 - Puzzle #01

---
+++ 2023/day-01-puzzle-01/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    let f = match File::open("input-01.txt") {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    let mut total = 0;
+
+    let reader = BufReader::new(f);
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        let mut first_digit: u32 = 0;
+        let mut final_digit: u32 = 0;
+        let mut first_digit_found = false;
+        let mut final_digit_found = false;
+        for ch in line.chars() {
+            if ch.is_alphabetic() {
+                continue;
+            }
+
+            if !first_digit_found {
+                first_digit = ch.to_digit(10).unwrap();
+                first_digit_found = true;
+                continue;
+            }
+
+            final_digit_found = true;
+            final_digit = ch.to_digit(10).unwrap();
+        }
+
+        if !final_digit_found {
+            final_digit = first_digit;
+        }
+
+        total += (first_digit * 10) + final_digit;
+
+        println!("{}", total);
+        println!("{}", line);
+        println!("{}{}", first_digit, final_digit);
+    }
+
+    println!("Total: {}", total);
+}
